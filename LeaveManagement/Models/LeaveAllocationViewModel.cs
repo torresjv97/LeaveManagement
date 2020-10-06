cd65f5f9fe1a83a6076149349007e8982758555c
Joshua Torres <torresjv1997@gmail.com>
2020-10-06 09:06:35 -0500

    Database updates

--- LeaveManagement/Models/LeaveAllocationViewModel.cs
+++ LeaveManagement/Models/LeaveAllocationViewModel.cs
@@ -12,15 +12,13 @@ namespace LeaveManagement.Models
     public class LeaveAllocationViewModel
     {
         public int Id { get; set; }
-        [Required]
         public int NumberOfDays { get; set; }
+        public int Period { get; set; }
         public DateTime DateCreated { get; set; }
         public Employee Employee { get; set; }
         public string EmployeeId { get; set; }
-
         public LeaveTypeViewModel LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
-
         public IEnumerable<SelectListItem> Employees { get; set; }
         public IEnumerable<SelectListItem> LeaveTypes { get; set; }
 
